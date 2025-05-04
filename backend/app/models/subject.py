5e5199a80c64e308951399b84cfc426a3a4fa78a
Paul200287 <paul.passath@gmail.com>
2025-05-04 19:40:24 +0200

    added the user id attribute

--- backend/app/models/subject.py
+++ backend/app/models/subject.py
@@ -9,13 +9,14 @@ class Subject(Base):
     __tablename__ = 'subjects'
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     name = Column(String(100), nullable=False)
     description = Column(String, nullable=True)
     semester = Column(String(20), nullable=True)
     teacher_name = Column(String(100), nullable=True)
-    color = Column(String(7), nullable=True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    deleted_at = Column(DateTime, nullable=True)
 
-    #users = relationship("SubjectUser", back_populates="subject")
+    user = relationship("User", back_populates="subject")
     exam = relationship("Exam", back_populates="subject")
