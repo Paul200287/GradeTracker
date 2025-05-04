06829feb5f53949bd683d6a573fb77d2652287f5
Paul200287 <paul.passath@gmail.com>
2025-05-04 19:39:18 +0200

    added subject route

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, user, exam
+from app.api.routes import login, user, exam, subject
 
 api_router = APIRouter()
 
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(exam.router, prefix="/exams", tags=["exams"])
+api_router.include_router(subject.router, prefix="/subjects", tags=["subjects"])
