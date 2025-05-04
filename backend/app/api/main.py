686dd2ce2dcd4b8b6e1c4c2e709fa6f21a03302b
Paul200287 <paul.passath@gmail.com>
2025-05-04 18:57:30 +0200

    router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, user
+from app.api.routes import login, user, exam
 
 api_router = APIRouter()
 
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(exam.router, prefix="/exams", tags=["exams"])
